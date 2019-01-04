4ff1f57c2793de85c7302109d25a646ca991984d
Ali Alsayed <a_alsayed87@hotmail.com>
2019-01-04 07:41:59 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    artists.each{|artist| artist.name}
+    artists.map{|artist| artist.name}
   end
 end
